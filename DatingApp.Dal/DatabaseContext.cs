09239f3fb5aece97f0118daf92f151473854118c
User <User@DESKTOP-7FO0HQM>
2023-08-01 13:08:36 +0300

    Добавьте файлы проекта.

---
+++ DatingApp.Dal/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DatingApp.Dal
+{
+    public class DatabaseContext: DbContext
+    {
+        public static IConfiguration GetConfiguration() =>
+            new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+
+            var optionsBuider = new DbContextOptionsBuilder<DatabaseContext>();
+            var connectionString = configuration.GetConnectionString("DatingApp");
+            optionsBuider.UseSqlServer(connectionString);
+        }
+    }
+}
