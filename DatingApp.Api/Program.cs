441c8c695183198fae645a7e12da9fc867b666af
Arina Romodanova <romodanova.arina@mail.ru>
2023-08-02 14:13:59 +0300

    Fixed connection dtrings in appsettings (DatingApp.Dal & DatingApp.Api)

--- DatingApp.Api/Program.cs
+++ DatingApp.Api/Program.cs
@@ -1,3 +1,6 @@
+using DatingApp.Dal.Repos;
+using DatingApp.Dal.Repos.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserRepo, UserRepo>();
 
 var app = builder.Build();
 
