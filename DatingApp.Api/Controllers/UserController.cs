9688fef074bf8d3f46df8f01ff03c0971e730750
Arina Romodanova <romodanova.arina@mail.ru>
2023-08-16 13:52:45 +0300

    Добавлена функция поиска пользователя по логину

--- DatingApp.Api/Controllers/UserController.cs
+++ DatingApp.Api/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using DatingApp.Dal.Repos.Base;
 using DatingApp.Dal.Repos.Interfaces;
 using DatingApp.Logging;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.Swagger.Annotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,6 +18,21 @@ namespace DatingApp.Api.Controllers
         public UserController(IUserRepo userRepo, IAppLogger<UserController> logger): base(userRepo, logger) 
         {
             
+        }
+
+        [HttpGet("[controller]/[action]/{login?}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [SwaggerResponse(200, "The execution was successful")]
+        [SwaggerResponse(400, "The execution was failed")]
+        public ActionResult GetUserByLogin(string? login)
+        {
+            if (!login.IsNullOrEmpty())
+            {
+                return Ok(((IUserRepo)MainRepo).GetUserByLogin(login));
+            }
+            Logger.LogAppError($"Invalid request to table");
+            return BadRequest();
         }
     }
 }
